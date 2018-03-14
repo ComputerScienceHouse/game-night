1ef9b4175e33c3b9d7c2fc276775e232c52e25e8
mfrancis95 <mikefrancis95@gmail.com>
2018-03-13 23:07:25 -0400

    Add main page template

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, render_template, request
 from pymongo import MongoClient
 from re import compile, IGNORECASE
 from random import choice
@@ -10,21 +10,25 @@ games = MongoClient().game_night.games
 
 @app.route('/api', methods = ['GET'])
 def all():
-    return jsonify(list(get_games()))
+    return jsonify(get_games())
 
 def get_games():
     filters = {}
     try:
-        filters['name'] = compile(request.args.get('name'), IGNORECASE)
+        filters['name'] = compile(request.args['name'], IGNORECASE)
     except:
         pass
     try:
-        players = int(request.args.get('players'))
+        players = int(request.args['players'])
         filters['$and'] = [{'min_players': {'$lte': players}}, {'max_players': {'$gte': players}}]
     except:
         pass
-    return games.find(filters, {'_id': False}).sort([('sortName', 1)])
+    return list(games.find(filters, {'_id': False}).sort([('sortName', 1)]))
+
+@app.route('/', methods = ['GET'])
+def index():
+    return render_template('index.html', games = get_games())
 
 @app.route('/api/random', methods = ['GET'])
 def random():
-    return jsonify(choice(list(get_games())))
+    return jsonify(choice(get_games()))
