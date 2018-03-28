def4cc04f1717a9300540a37b1a5d69af6e5a279
mfrancis95 <mikefrancis95@gmail.com>
2018-03-28 12:59:03 -0400

    Integrate OIDC authentication

--- app.py
+++ app.py
@@ -1,24 +1,37 @@
 from flask import Flask, jsonify, render_template, request
+from os import environ
+from flask_pyoidc.flask_pyoidc import OIDCAuthentication
 from pymongo import MongoClient
 from re import compile, IGNORECASE
 from random import choice
 
 app = Flask(__name__)
+app.config.update({'SERVER_NAME': environ['GAME_NIGHT_SERVER_NAME']})
 app.jinja_env.lstrip_blocks = True
 app.jinja_env.trim_blocks = True
+app.secret_key = environ['GAME_NIGHT_SECRET_KEY']
 app.url_map.strict_slashes = False
+
+client_info = {
+    'client_id': environ['GAME_NIGHT_CLIENT_ID'],
+    'client_secret': environ['GAME_NIGHT_CLIENT_SECRET']
+}
+auth = OIDCAuthentication(app, client_registration_info = client_info, issuer = environ['GAME_NIGHT_ISSUER'])
 
 games = MongoClient().game_night.games
 
 @app.route('/api')
+@auth.oidc_auth
 def api():
     return jsonify(get_games())
 
 @app.route('/api/random')
+@auth.oidc_auth
 def api_random():
     return jsonify(choice(get_games()))
 
 @app.route('/api/count')
+@auth.oidc_auth
 def api_count():
     return jsonify(get_games(True))
 
@@ -41,9 +54,11 @@ def get_players():
     return list(games.aggregate([{'$group': {'_id': False, 'max': {'$max': '$max_players'}, 'min': {'$min': '$min_players'}}}]))[0]
 
 @app.route('/')
+@auth.oidc_auth
 def index():
     return render_template('index.html', games = get_games(), players = get_players())
 
 @app.route('/random')
+@auth.oidc_auth
 def random():
     return render_template('index.html', games = [choice(get_games())], players = get_players())
