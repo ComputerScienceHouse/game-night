9208c8b7dcd5875f9e6a46dd53a96012f06ec053
mfrancis95 <mikefrancis95@gmail.com>
2018-04-08 14:46:21 -0400

    Improve card format and layout, resolve #3 and start submissions

--- game_night/db.py
+++ game_night/db.py
@@ -1,9 +1,13 @@
 from pymongo import MongoClient
 from re import compile, IGNORECASE
+from flask import request, session
 
-games = MongoClient().game_night.games
+game_night = MongoClient().game_night
+gamemasters = game_night.gamemasters
+games = game_night.games
+submissions = game_night.submissions
 
-def create_filters(request):
+def create_filters():
     filters = {}
     try:
         filters['name'] = compile(request.args['name'], IGNORECASE)
@@ -16,14 +20,20 @@ def create_filters(request):
         pass
     return filters
 
-def get_count(request):
-    return games.count(create_filters(request))
+def get_count():
+    return games.count(create_filters())
 
-def get_games(request):
-    return games.find(create_filters(request), {'_id': False}).sort([('sort_name', 1)])
+def get_games():
+    return games.find(create_filters(), {'_id': False}).sort([('sort_name', 1)])
 
 def get_players():
     return games.aggregate([{'$group': {'_id': False, 'max': {'$max': '$max_players'}, 'min': {'$min': '$min_players'}}}]).next()
 
-def get_random_game(request):
-    return games.aggregate([{'$match': create_filters(request)}, {'$sample': {'size': 1}}, {'$project': {'_id': False}}]).next()
+def get_random_game():
+    return games.aggregate([{'$match': create_filters()}, {'$sample': {'size': 1}}, {'$project': {'_id': False}}]).next()
+
+def get_submissions(gamemaster = False):
+    return submissions.find() if gamemaster else submissions.find({'username': session['userinfo']['preferred_username']})
+
+def is_gamemaster():
+    return gamemasters.count({'username': session['userinfo']['preferred_username']})
