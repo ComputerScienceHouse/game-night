e929dd109e917326759d16f39e5127410bc09598
mfrancis95 <mikefrancis95@gmail.com>
2018-10-13 18:15:16 -0400

    Change BGG link pattern to be more accomodating (fix #44)

--- game_night/game.py
+++ game_night/game.py
@@ -12,7 +12,7 @@ def _unique(form, field):
 class Game(FlaskForm):
 
     image = FileField('image', validators = [FileRequired(), FileAllowed(['jpg'])])
-    link = StringField('link', validators = [DataRequired(), Regexp('https://boardgamegeek.com/boardgame/.*')])
+    link = StringField('link', validators = [DataRequired(), Regexp('https://boardgamegeek.com/.*')])
     max_players = IntegerField('max_players', validators = [DataRequired()])
     min_players = IntegerField('min_players', validators = [DataRequired()])
     name = StringField('name', validators = [DataRequired(), _unique])
